315c873c2a48e0252a8959d6b227f16640b5d6b0
Del Turner <planeguy@delek.ca>
2021-05-04 15:50:41 -0700

    clean up server

--- server.js
+++ server.js
@@ -1,36 +1,23 @@
+/* ===== command line options ==== */
+const {Command} = require('commander');
+const program = new Command();
+program.version('0.0.1');
+
+program
+    .option('-t, --title <title>', 'chat title', 'Simplest Chat')
+
+program.parse(process.argv);
+const options = program.opts();
+
+/* ===== set up service ===== */
 const Koa = require('koa');
 const IO = require('koa-socket-2');
 
 const app = new Koa();
 const io = new IO();
 
-const colours = {
-    Reset : "\x1b[0m",
-    Bright : "\x1b[1m",
-    Dim : "\x1b[2m",
-    Underscore : "\x1b[4m",
-    Blink : "\x1b[5m",
-    Reverse : "\x1b[7m",
-    Hidden : "\x1b[8m",
-
-    FgBlack : "\x1b[30m",
-    FgRed : "\x1b[31m",
-    FgGreen : "\x1b[32m",
-    FgYellow : "\x1b[33m",
-    FgBlue : "\x1b[34m",
-    FgMagenta : "\x1b[35m",
-    FgCyan : "\x1b[36m",
-    FgWhite : "\x1b[37m",
-
-    BgBlack : "\x1b[40m",
-    BgRed : "\x1b[41m",
-    BgGreen : "\x1b[42m",
-    BgYellow : "\x1b[43m",
-    BgBlue : "\x1b[44m",
-    BgMagenta : "\x1b[45m",
-    BgCyan : "\x1b[46m",
-    BgWhite : "\x1b[47m"
-}
+/* ===== colours stuff ===== */
+const resetColour = "\x1b[0m";
 const usercolours = [
     "\x1b[31m",
     "\x1b[32m",
@@ -38,9 +25,12 @@ const usercolours = [
     "\x1b[34m",
     "\x1b[35m",
     "\x1b[36m",
-]
+];
+
+/* ===== user list ===== */
 let users = {};
 
+/* ===== network events ===== */
 io.attach(app);
 
 io.on('connect', (ctx, data)=>{
@@ -50,21 +40,20 @@ io.on('connect', (ctx, data)=>{
         node:ctx.handshake.query.node,
         colour: usercolours[Math.floor(Math.random() * usercolours.length)]
     };
+    //tell everyone they've arrived
     io.broadcast('message',{
-        message: `${users[ctx.id].colour}${ctx.handshake.query.user}${colours.Reset} on node ${ctx.handshake.query.node} has arrived!`
+        message: `${users[ctx.id].colour}${ctx.handshake.query.user}${resetColour} on node ${ctx.handshake.query.node} has arrived!`
     });
-    ctx.emit('message', {
-        serverconnect:{
-            colour: users[ctx.id].colour
-        }
+    //send the user server information
+    ctx.emit('serverconnect', {
+        colour: users[ctx.id].colour,
+        title: options.title
     });
 });
 
 io.on('message', (ctx, data)=>{
-    //announce the message to the rest of the room
-    console.log(`${users[ctx.socket.id].user}: ${data.message}`);
-    //io.broadcast
-    ctx.socket.broadcast.emit('message',{
+    //send the message to all users including the sendre (so it shows up in their chat list)
+    io.broadcast('message',{
         user: users[ctx.socket.id].user, 
         node: users[ctx.socket.id].node, 
         colour: users[ctx.socket.id].colour, 
@@ -73,12 +62,14 @@ io.on('message', (ctx, data)=>{
 });
 
 io.on('disconnect', (ctx, data)=>{
-    //announce the chatter departure
+    //announce the user's departure
     io.broadcast('message',{
         message:`${users[ctx.socket.id].user} has left.`
     });
-    console.log(`${users[ctx.socket.id].user} has left.`);
+    // remove the user from the list
     delete users[ctx.socket.id];
 });
 
-app.listen(process.env.PORT||10101);
+
+// ===== START SERVER =====
+app.listen(10101);
